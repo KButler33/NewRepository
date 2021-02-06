177724fd586f85c083f41cb16c9361845f257ba9
KButler33 <78574796+KButler33@users.noreply.github.com>
2021-02-06 16:53:35 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var itemPrice = 15.99;
+var amountOfMoneyInWallet = 50;
+var numberOfFriends = 12;
+var ageInYears = 37;
+var firstName = "Kathryn";
+var lastName = "Butler";
+var middleInitial = " A ";
+
+console.log ("Amount of money in wallet after buying the item:" , amountOfMoneyInWallet - itemPrice );
+
+console.log ("Number of friends made each year based on age:" , numberOfFriends / ageInYears );
+
+console.log ("Full Name:" , firstName + middleInitial + lastName );
